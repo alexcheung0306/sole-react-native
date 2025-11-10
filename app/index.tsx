6dc7f0755f54c41b8a7254167302b01395631e93
carson <leelokhin0406@gmail.com>
2025-11-10 11:10:11 +0800

    fix: config for emulator fetching api

---
+++ app/index.tsx
@@ -0,0 +1,7 @@
+import { Redirect } from 'expo-router';
+
+export default function Page() {
+  // Redirect to the protected user home screen
+  return <Redirect href="/(protected)/(user)/home" />;
+}
+
